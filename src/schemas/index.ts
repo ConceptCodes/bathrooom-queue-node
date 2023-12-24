c4f3674631b44a1b79561c66e6737d9807f63c68
concept <conceptcodes@gmail.com>
2023-12-24 05:34:34 -0500

    feat: added logger for sql queries

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,12 +2,23 @@ import { z } from "zod";
 import { createInsertSchema } from "drizzle-zod";
 
 import { userTable, insertUserSchema } from "@lib/db/schema";
+import { env } from "@lib/env";
 
 // Auth
-export const loginSchema = insertUserSchema.pick({
-  email: true,
-  password: true,
-});
+export const loginSchema = createInsertSchema(userTable, {
+  email: (schema) => schema.email.email(),
+  password: (schema) => schema.password.min(8),
+})
+  .pick({
+    email: true,
+    password: true,
+  })
+  .refine((data) => {
+    if (!env.COMPANY_EMAIL_ID) return true;
+    return (
+      data.email.substring(data.email.indexOf("@")) === env.COMPANY_EMAIL_ID
+    );
+  });
 
 export const registerSchema = insertUserSchema
   .pick({
@@ -41,7 +52,6 @@ export const updateSchema = createInsertSchema(userTable, {
 export const onlyEmailSchema = z.object({
   email: z.string().email(),
 });
-
 
 export type LoginSchema = z.infer<typeof loginSchema>;
 export type ForgotPasswordSchema = z.infer<typeof forgotPasswordSchema>;
